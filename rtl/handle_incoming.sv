54ea750d8f3ebb92225670a94456e8409c2b9f8d
Joseph Zuckerman <jzuck@cs.columbia.edu>
2019-09-24 11:13:26 -0400

    handle_incoming: removed flops

--- rtl/handle_incoming.sv
+++ rtl/handle_incoming.sv
@@ -23,11 +23,7 @@ module handle_incoming(clk, rst, llc_rst_tb_valid, llc_rsp_in_vaild, llc_req_in_
     logic is_rst_to_get, is_rsp_to_get,  is_req_to_get, is_dma_req_to_get; 
     logic req_in_stalled_valid; 
 
-    logic is_rst_to_resume_next, is_flush_to_resume_next, is_dma_read_to_resume_next, is_dma_write_to_resume_next;
-    logic is_rst_to_get_next, is_rsp_to_get_next,  is_req_to_get_next, is_dma_req_to_get_next; 
-    logic req_in_stalled_valid_next, update_req_in_from_stalled_next;
-
-    logic do_get_req, do_get_req_next, do_get_dma_req, do_get_dma_req_next; 
+    logic do_get_req, do_get_dma_req; 
 
     always_ff @(posedge clk or negedge rst) begin 
         if (!rst) begin 
@@ -35,86 +31,67 @@ module handle_incoming(clk, rst, llc_rst_tb_valid, llc_rsp_in_vaild, llc_req_in_
            can_get_rsp_in <= 1'b0; 
            can_get_req_in <= 1'b0; 
            can_get_dma_req_in <= 1'b0; 
-           is_rst_to_resume <= 1'b0; 
-           is_flush_to_resume <= 1'b0; 
-           is_dma_read_to_resume <= 1'b0; 
-           is_dma_write_to_resume <= 1'b0; 
-           is_rst_to_get <= 1'b0; 
-           is_rsp_to_get <= 1'b0; 
-           is_req_to_get <= 1'b0; 
-           is_dma_req_to_get <= 1'b0;
            req_in_stalled_valid <= 1'b0;
-           do_get_req <= 1'b0;
-           do_get_dma_req <= 1'b0; 
         end else begin 
            can_get_rst_tb <= llc_rst_tb_valid; 
            can_get_rsp_in <= llc_rsp_in_valid; 
            can_get_req_in <= llc_req_in_valid; 
            can_get_dma_req_in <= llc_dma_req_in_valid;
-           is_rst_to_resume <= is_rst_to_resume_next; 
-           is_flush_to_resume <= is_flush_to_resume_next;  
-           is_dma_read_to_resume <= is_dma_read_to_resume_next;
-           is_dma_wrie_to_resume <= is_dma_write_to_resume_next;  
-           is_rst_to_get <= is_rst_to_get_next;  
-           is_rsp_to_get <= is_rsp_to_get_next; 
-           is_req_to_get <= is_req_to_get_next; 
-           is_dma_req_to_get <= is_dma_req_to_get_next; 
            req_in_stalled_valid <= req_in_stalled_valid_next;
-           do_get_req <= do_get_req_next;  
-           do_get_dma_req <= do_get_dma_req_next;
         end
     end
 
     always_comb begin  
-        is_rst_to_resume_next =  1'b0; 
-        is_flush_to_resume_next = 1'b0; 
-        is_rst_to_get_next = 1'b0; 
-        is_rsp_to_get_next = 1'b0;  
-        is_req_to_get_next = 1'b0;  
-        is_dma_req_to_get_next =  1'b0;  
-        is_dma_read_to_resume_next = 1'b0; 
-        is_dma_write_to_resume_next = 1'b0; 
+        is_rst_to_resume =  1'b0; 
+        is_flush_to_resume = 1'b0; 
+        is_rst_to_get = 1'b0; 
+        is_rsp_to_get = 1'b0;  
+        is_req_to_get = 1'b0;  
+        is_dma_req_to_get =  1'b0;  
+        is_dma_read_to_resume = 1'b0; 
+        is_dma_write_to_resume = 1'b0; 
         req_in_stalled_valid_next = 1'b0; 
-        do_get_req_next = 1'b0;  
-        update_req_in_from_stalled_next = 1'b0; 
+        do_get_req = 1'b0; 
+        do_get_dma_req = 1'b0;  
+        update_req_in_from_stalled = 1'b0; 
 
         if (recall_pending) begin 
             if(!recall_valid) begin 
                 if(can_get_rsp_in) begin 
-                    is_rsp_to_get_next = 1'b1; 
+                    is_rsp_to_get = 1'b1; 
                 end 
             end else begin 
                 if (dma_read_pending) begin 
-                    is_dma_read_to_resume_next = 1'b1; 
+                    is_dma_read_to_resume = 1'b1; 
                 end else if (dma_write_pending) begin 
-                    is_dma_write_to_resume_next = 1'b1; 
+                    is_dma_write_to_resume = 1'b1; 
                 end
             end
         end else if (rst_stall) begin 
-             is_rst_to_resume_next = 1'b1; 
+            is_rst_to_resume = 1'b1; 
         end else if (flush_stall) begin
-            is_flush_to_resume_next = 1'b1; 
+            is_flush_to_resume = 1'b1; 
         end else if (can_get_rst_tb && !dma_read_pending && !dma_write_pending) begin 
-            is_rst_to_get_next = 1'b1;
+            is_rst_to_get = 1'b1;
         end else if (can_get_rsp_in) begin 
-            is_rsp_to_get_next =  1'b1;
+            is_rsp_to_get =  1'b1;
         end else if ((can_get_req_in &&  !req_stall)  ||  (!req_stall  && req_in_stalled_valid)) begin 
             if (req_in_stalled_valid) begin 
                 req_in_stalled_vaild_next = 1'b0;
-                update_req_in_from_stalled_next = 1'b1;   
+                update_req_in_from_stalled = 1'b1;   
             end else
-                do_get_req_next = 1'b1;
+                do_get_req = 1'b1;
             end
-            is_req_to_get_next = 1'b1;
+            is_req_to_get = 1'b1;
         end else if (dma_read_pending) begin 
-            is_dma_read_to_resume_next = 1'b1; 
+            is_dma_read_to_resume = 1'b1; 
         end else if (dma_write_pending) begin 
             if (can_get_dma_req_in) begin 
-                is_dma_write_to_resume_next = 1'b1; 
-                do_get_dma_req_next = 1'b1;
+                is_dma_write_to_resume = 1'b1; 
+                do_get_dma_req = 1'b1;
             end
         end else if (can_get_dma_req_in && !req_stall) begin 
-            is_dma_req_to_get_next = 1'b1; 
+            is_dma_req_to_get = 1'b1; 
             do_get_dma_req = 1'b1;
         end
     end
