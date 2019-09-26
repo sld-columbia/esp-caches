4e0ee4883411925ffc95b57f14fc1b3c1e04d1d5
Joseph Zuckerman <zuck@noiseaware.io>
2019-09-26 19:35:18 -0400

    renamed decoder, added logic for set computation

--- rtl/input_decoder.sv
+++ rtl/input_decoder.sv
@@ -2,28 +2,34 @@
 `include "cache_consts.svh" 
 `include "cache_types.svh" 
 
-// handle_incoming.sv 
+// input_decoder.sv 
 // Author: Joseph Zuckerman
 // processes available incoming signals with priority 
 
-module handle_incoming(clk, rst, llc_rst_tb_valid, llc_rsp_in_vaild, llc_req_in_valid, llc_dma_req_in_valid, recall_pending, recall_valid, dma_read_pending, dma_write_pending, flush_stall, rst_stall, req_stall, update_req_in_from_stalled, llc_rst_tb_ready, llc_rsp_in_ready, llc_req_in_ready, llc_dma_req_in_ready); 
+module input_decoder (clk, rst, llc_rst_tb_valid, llc_rsp_in_vaild, llc_req_in_valid, llc_dma_req_in_valid, recall_pending, recall_valid, dma_read_pending, dma_write_pending, flush_stall, rst_stall, req_stall, update_req_in_from_stalled, llc_rst_tb_ready, llc_rsp_in_ready, llc_req_in_ready, llc_dma_req_in_ready, look); 
    
     input logic clk, rst; 
     input logic llc_rst_tb_valid, llc_rsp_in_vaild, llc_req_in_valid, llc_dma_req_in_valid; 
     input logic recall_pending, recall_valid;
     input logic dma_read_pending, dma_write_pending; 
     input logic flush_stall, rst_stall, req_stall; 
-
-    output logic update_req_in_from_stalled;  
+    input logic req_in_stalled_valid;
+    input llc_set_t rst_flush_stalled_set, req_in_stalled_set;
+    input llc_tag_t req_in_stalled_tag; 
+    input llc_addr_t rsp_in_addr, req_in_addr, dma_req_in_addr, dma_addr; 
+    
+    output logic update_req_in_from_stalled, clr_req_in_stalled_valid;  
     output logic llc_rst_tb_ready, llc_rsp_in_ready, llc_req_in_ready, llc_dma_req_in_ready; 
+    output logic look;
+    output llc_set_t set;
+    output logic incr_rst_flush_stalled_set; 
+    output logic clr_rst_stall, clr_flush_stall, clr_req_stall;
 
     logic can_get_rst_tb, can_get_rsp_in, can_get_req_in, can_get_dma_req_in; 
-    
     logic is_rst_to_resume, is_flush_to_resume, is_dma_read_to_resume, is_dma_write_to_resume;
     logic is_rst_to_get, is_rsp_to_get,  is_req_to_get, is_dma_req_to_get; 
-    logic req_in_stalled_valid; 
-
     logic do_get_req, do_get_dma_req; 
+    llc_addr_t addr_for_set; 
 
     always_ff @(posedge clk or negedge rst) begin 
         if (!rst) begin 
@@ -53,7 +59,8 @@ module handle_incoming(clk, rst, llc_rst_tb_valid, llc_rsp_in_vaild, llc_req_in_
         req_in_stalled_valid_next = 1'b0; 
         do_get_req = 1'b0; 
         do_get_dma_req = 1'b0;  
-        update_req_in_from_stalled = 1'b0; 
+        update_req_in_from_stalled = 1'b0;
+        clr_req_in_stalled_valid = 1'b0;
 
         if (recall_pending) begin 
             if(!recall_valid) begin 
@@ -77,7 +84,7 @@ module handle_incoming(clk, rst, llc_rst_tb_valid, llc_rsp_in_vaild, llc_req_in_
             is_rsp_to_get =  1'b1;
         end else if ((can_get_req_in &&  !req_stall)  ||  (!req_stall  && req_in_stalled_valid)) begin 
             if (req_in_stalled_valid) begin 
-                req_in_stalled_vaild_next = 1'b0;
+                clr_req_in_stalled_valid = 1'b1;
                 update_req_in_from_stalled = 1'b1;   
             end else
                 do_get_req = 1'b1;
@@ -100,5 +107,44 @@ module handle_incoming(clk, rst, llc_rst_tb_valid, llc_rsp_in_vaild, llc_req_in_
     assign llc_rst_tb_ready = is_rst_to_get; 
     assign llc_req_in_ready = do_get_req;
     assign llc_dma_req_in = do_get_dma_req; 
+
+    assign look =  is_flush_to_resume | is_rsp_to_get | 
+                   is_req_to_get | is_dma_req_to_get | 
+                   (is_dma_read_to_resume & ~recall_pending) | 
+                   (is_dma_write_to_resule & ~recall_pendind); 
+    
+    always_comb begin  
+        addr_for_set = {LLC_ADDR_BITS{1'b0}};
+        if (is_rsp_to_get) begin 
+            addr_for_set = rsp_in_addr; 
+        end else if (is_req_to_get) begin 
+            addr_for_set = req_in_addr;
+        end else if (is_dma_req_to_get  || is_dma_read_to_resume || is_dma_write_to_resume) begin 
+            addr_for_set = is_dma_req_to_get ? dma_req_in_addr : dma_addr; 
+        end
+    end
+
+    assign set = (is_flush_to_resume | is_rst_to_resume) ? rst_flush_stalled_set : addr_for_set[(LLC_SET_BITS-1):0]; 
+
+    always_comb begin
+        incr_flush_stalled_set = 1'b0;
+        clr_rst_stall = 1'b0;
+        clr_flush_stall = 1'b0; 
+        if (is_flush_to_resume || is_rst_to_resume) begin 
+            incr_flush_stalled_set = 1'b1;
+            if (rst_flush_stalled_set == {LLC_SET_BITS{1'b1}}) begin 
+                clr_rst_stall  =  1'b1; 
+                clr_flush stall = 1'b1; 
+            end    
+        end else if (is_rsp_to_get) begin 
+            if ((req_stall == 1'b1) 
+                &&(addr_for_set[(ADDR_BITS - OFFSET_BITS - 1): LLC_SET_BITS] == req_in_stalled_tag) 
+                && (addr_for_set[(LLC_SET_BITS-1):0] == req_in_stalled_set)) begin 
+                clr_req_stall = 1'b1;
+            end
+        end            
+    end 
+
+
 
 endmodule
