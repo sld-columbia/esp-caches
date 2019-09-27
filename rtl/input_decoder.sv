61f8263b5f90932412b1a05ec278b44f5b36b33a
Joseph Zuckerman <zuck@noiseaware.io>
2019-09-27 16:11:28 -0400

    llc.sv: reset, set,  and clear of global signals

--- rtl/input_decoder.sv
+++ rtl/input_decoder.sv
@@ -6,7 +6,7 @@
 // Author: Joseph Zuckerman
 // processes available incoming signals with priority 
 
-module input_decoder (clk, rst, llc_rst_tb_valid, llc_rsp_in_vaild, llc_req_in_valid, llc_dma_req_in_valid, recall_pending, recall_valid, dma_read_pending, dma_write_pending, flush_stall, rst_stall, req_stall, update_req_in_from_stalled, llc_rst_tb_ready, llc_rsp_in_ready, llc_req_in_ready, llc_dma_req_in_ready, look); 
+module input_decoder (clk, rst, llc_rst_tb_valid, llc_rsp_in_vaild, llc_req_in_valid, llc_dma_req_in_valid, recall_pending, recall_valid, dma_read_pending, dma_write_pending, flush_stall, rst_stall, req_stall, req_in_stalled_valid, decode_en, rst_flush_stalled_set, req_in_stalled_set, req_in_stalled_tag, rsp_in_addr, req_in_addr, dma_req_in_addr, dma_addr, update_req_in_from_stalled, llc_rst_tb_ready, llc_rsp_in_ready, llc_req_in_ready, llc_dma_req_in_ready, look, set, incr_rst_flush_stalled_set, clr_rst_stall, clr_flush_stall, clr_req_stall); 
    
     input logic clk, rst; 
     input logic llc_rst_tb_valid, llc_rsp_in_vaild, llc_req_in_valid, llc_dma_req_in_valid; 
@@ -17,7 +17,8 @@ module input_decoder (clk, rst, llc_rst_tb_valid, llc_rsp_in_vaild, llc_req_in_v
     input logic decode_en; 
     input llc_set_t rst_flush_stalled_set, req_in_stalled_set;
     input llc_tag_t req_in_stalled_tag; 
-    input llc_addr_t rsp_in_addr, req_in_addr, dma_req_in_addr, dma_addr; 
+    input line_addr_t rsp_in_addr, req_in_addr, dma_req_in_addr;
+    input addr_t dma_addr; 
     
     output logic update_req_in_from_stalled, clr_req_in_stalled_valid;  
     output logic llc_rst_tb_ready, llc_rsp_in_ready, llc_req_in_ready, llc_dma_req_in_ready; 
@@ -25,14 +26,14 @@ module input_decoder (clk, rst, llc_rst_tb_valid, llc_rsp_in_vaild, llc_req_in_v
     output llc_set_t set;
     output logic incr_rst_flush_stalled_set; 
     output logic clr_rst_stall, clr_flush_stall, clr_req_stall;
+    output logic update_dma_addr_from_req; 
 
-    llc_addr_t addr_for_set; 
-    line_breakdown_llc_t line_br; 
+    //STATE LOGIC
 
-    localparam IDLE = 2'b00;  
-    localparam DECODE = 2'b01; 
-    localparam SET = 2'b11;
-    localparam STALL = 2'b10; 
+    localparam IDLE = 2'b00; //waiting for start  
+    localparam DECODE = 2'b01; //determining action
+    localparam SET = 2'b11; //computing set
+    localparam STALL = 2'b10; //clearing stalls  
     logic[1:0] state, next_state;
     
     always_ff @(posedge clk or negedge rst) begin 
@@ -56,6 +57,7 @@ module input_decoder (clk, rst, llc_rst_tb_valid, llc_rsp_in_vaild, llc_req_in_v
                 next_state = STALL;
             STALL:
                 next_state = IDLE;
+        endcase 
     end
 
     logic st_dec, st_set, st_stall; 
@@ -79,7 +81,9 @@ module input_decoder (clk, rst, llc_rst_tb_valid, llc_rsp_in_vaild, llc_req_in_v
            req_in_stalled_valid <= req_in_stalled_valid_next;
         end
     end
-    
+   
+    //DECODE 
+
     logic is_rst_to_resume, is_flush_to_resume, is_dma_read_to_resume, is_dma_write_to_resume;
     logic is_rst_to_get, is_rsp_to_get,  is_req_to_get, is_dma_req_to_get; 
     logic do_get_req, do_get_dma_req; 
@@ -89,6 +93,7 @@ module input_decoder (clk, rst, llc_rst_tb_valid, llc_rsp_in_vaild, llc_req_in_v
     logic do_get_req_next, do_get_dma_req_next; 
     logic update_req_in_from_stalled_next, clr_req_in_stalled_valid_next; 
 
+    //decoder logic
     always_comb begin  
         is_rst_to_resume_next =  1'b0; 
         is_flush_to_resume_next = 1'b0; 
@@ -144,6 +149,7 @@ module input_decoder (clk, rst, llc_rst_tb_valid, llc_rsp_in_vaild, llc_req_in_v
         end
     end
 
+    //flop outputs 
     always_ff@(posedge clk or negedge rst) begin 
         if (!rst) begin 
             is_rst_to_resume <= 1'b0; 
@@ -174,6 +180,7 @@ module input_decoder (clk, rst, llc_rst_tb_valid, llc_rsp_in_vaild, llc_req_in_v
         end
     end
 
+    //assign ready bits for top level
     assign llc_rsp_in_ready = is_rsp_to_get; 
     assign llc_rst_tb_ready = is_rst_to_get; 
     assign llc_req_in_ready = do_get_req;
@@ -184,39 +191,54 @@ module input_decoder (clk, rst, llc_rst_tb_valid, llc_rsp_in_vaild, llc_req_in_v
                    (is_dma_read_to_resume & ~recall_pending) | 
                    (is_dma_write_to_resule & ~recall_pendind); 
     
+    //SET
+    
+    llc_addr_t addr_for_set; 
+    line_breakdown_llc_t line_br; 
+    
+    logic update_dma_addr_from_req_next; 
+    //multiplex set bits
     always_comb begin  
         addr_for_set = {LLC_ADDR_BITS{1'b0}};
+        update_dma_addr_from_req_next = 1'b0;
         if (is_rsp_to_get) begin 
             addr_for_set = rsp_in_addr; 
         end else if (is_req_to_get) begin 
             addr_for_set = req_in_addr;
         end else if (is_dma_req_to_get  || is_dma_read_to_resume || is_dma_write_to_resume) begin 
             addr_for_set = is_dma_req_to_get ? dma_req_in_addr : dma_addr; 
+            update_dma_addr_from_req_next = 1'b0;
         end
     end
     
+    //flop outputs
     always_ff @(posedge clk or negedge rst) begin 
         if (!rst) begin 
             line_br.tag <= 0; 
             line_br.set <= 0;
+            update_dma_addr_from_req <= 1'b0;
         end else if (st_decode) begin 
             line_br.tag <= addr_for_set[(ADDR_BIT - OFFSET_BITS -1): LLC_SET_BITS];
             line_br.set <= addr_for_set[(LLC_SET_BITS - 1):0]; 
+            update_dma_addr_from_req <= update_dma_addr_from_req_next; 
         end
     end
 
     assign set = (is_flush_to_resume | is_rst_to_resume) ? rst_flush_stalled_set : line_br.set; 
     
+    //STALL
+
     logic incr_rst_flush_stalled_set_next; 
     logic clr_rst_stall_next, clr_flush_stall_next, clr_req_stall_next;
 
+    //set bits for clearing signals
     always_comb begin
-        incr_flush_stalled_set_next = 1'b0;
+        incr_rst_flush_stalled_set_next = 1'b0;
         clr_rst_stall_next = 1'b0;
         clr_flush_stall_next = 1'b0; 
         clr_req_stall_next = 1'b0;
         if (is_flush_to_resume || is_rst_to_resume) begin 
-            incr_flush_stalled_set_next = 1'b1;
+            incr_rst_flush_stalled_set_next = 1'b1;
             if (rst_flush_stalled_set == {LLC_SET_BITS{1'b1}}) begin 
                 clr_rst_stall_next  =  1'b1; 
                 clr_flush stall = 1'b1; 
@@ -227,21 +249,21 @@ module input_decoder (clk, rst, llc_rst_tb_valid, llc_rsp_in_vaild, llc_req_in_v
                 && (line_br.set == req_in_stalled_set)) begin 
                 clr_req_stall_next = 1'b1;
             end
-        end            
+        end
     end 
 
+    //flop outputs
     always_ff @(posedge clk or negedge rst) begin 
         if (!rst) begin 
-            incr_flush_stalled_set <= 1'b0;
+            incr_rst_flush_stalled_set <= 1'b0;
             clr_rst_stall <= 1'b0; 
             clr_flush_stall <= 1'b0; 
             clr_req_stall <= 1'b0; 
         end else if (st_stall) begin
-            incr_flush_stalled_set <= incr_flush_stalled_set_next; 
+            incr_rst_flush_stalled_set <= incr_rst_flush_stalled_set_next; 
             clr_rst_stall <= clr_rst_stall_next;
             clr_flush_stall <= clr_flush_stall_next;
             clr_req_stall <= clr_req_stall_next;
         end
     end
 endmodule
-
--- rtl/llc.sv
+++ rtl/llc.sv
@@ -8,7 +8,7 @@
 
 module llc(clk, rst, llc_req_in_i, llc_req_in_valid, llc_req_in_ready, llc_dma_req_in_i, llc_dma_req_in_valid, llc_dma_reqin_ready, llc_rsp_in_i, llc_rsp_in_valid, llc_rsp_in_ready, llc_mem_rsp_i, llc_mem_rsp_valid, llc_mem_rsp_ready, llc_rst_tb_i, llc_rst_tb_valid, llc_rst_tb_ready, llc_rsp_out_ready, llc_rsp_out_valid, llc_rsp_out, llc_dma_rsp_out_ready, llc_dma_rsp_out_valid, llc_dma_rsp_out, llc_fwd_out_ready, llc_fwd_out_valud, llc_fwd_out, llc_mem_req_ready,  llc_mem_req_valid, llc_mem_req, llc_rst_tb_done_ready, llc_rst_tb_done_valid, llc_rst_tb_done
 `ifdef STATS_ENABLE
-	, llc_stats_ready, llc_stats_valud, llc_stats
+	, llc_stats_ready, llc_stats_valid, llc_stats
 `endif
 );
 
@@ -109,4 +109,100 @@ module llc(clk, rst, llc_req_in_i, llc_req_in_valid, llc_req_in_ready, llc_dma_r
         end
     end
     
+    logic rst_stall, clr_rst_stall, set_rst_stall;
+    always @(posedge clk or negedge rst) begin 
+        if (!rst || set_rst_stall) begin 
+            rst_stall <= 1'b1;
+        end else if (clr_rst_stall) begin 
+            rst_stall <= 1'b0;
+        end
+    end
+
+    logic flush_stall, clr_flush_stall, set_flush_stall; 
+    always @(posedge clk or negedge rst) begin 
+        if (!rst || clr_flush_stall) begin 
+            flush_stall <= 1'b0; 
+        end else if (set_flush_stall) begin 
+            flush_stall <= 1'b1; 
+        end
+    end
+
+    logic req_stall, clr_req_stall, set_req_stall; 
+    always @(posedge clk or negedge rst) begin 
+        if (!rst || clr_req_stall) begin 
+            req_stall <= 1'b0; 
+        end else if (set_req_stall) begin 
+            req_stall <= 1'b1; 
+        end
+    end
+
+    logic req_in_stalled_valid, clr_req_in_stalled_valid, set_req_in_stalled_valid;  
+     always @(posedge clk or negedge rst) begin 
+        if (!rst || clr_req_in_stalled_valid) begin 
+            req_in_stalled_valid <= 1'b0; 
+        end else if (set_req_in_stalled_valid) begin 
+            req_in_stalled_valid <= 1'b1; 
+        end
+    end
+
+    llc_set_t rst_flush_stalled_set;
+    logic clr_rst_flush_stalled_set, incr_rst_flush_stalled_set;
+    always @(posedge clk or negedge rst) begin 
+        if (!rst || clr_rst_flush_stalled_set) begin 
+            rst_flush_stalled_set <= 0; 
+        end else if (incr_rst_flush_stalled_set) begin 
+            rst_flush_stalled_set <= rst_flush_stalled_set + 1; 
+        end
+    end
+    
+    line_addr_t dma_addr;
+    logic update_dma_addr_from_req; 
+    always @(posedge clk or negedge rst) begin 
+        if (!rst) begin 
+            dma_addr <= 0; 
+        end else if (update_dma_addr_from_req) begin 
+            dma_addr <= dma_req_in.addr;
+        end
+    end
+
+    logic recall_pending, clr_recall_pending, set_recall_pending;    
+    always @(posedge_clk or negedge rst) begin 
+        if (!rst || clr_recall_pending) begin 
+            recall_pending <= 1'b0;
+        end else if (set_recall_pending) begin 
+            recall_pending <= 1'b1;
+        end
+    end
+
+    logic dma_read_pending, clr_dma_read_pending, set_dma_read_pending;    
+    always @(posedge_clk or negedge rst) begin 
+        if (!rst || clr_dma_read_pending) begin 
+            dma_read_pending <= 1'b0;
+        end else if (set_dma_read_pending) begin 
+            dma_read_pending <= 1'b1;
+        end
+    end
+
+    logic dma_write_pending, clr_dma_write_pending, set_dma_write_pending;    
+    always @(posedge_clk or negedge rst) begin 
+        if (!rst || clr_dma_write_pending) begin 
+            dma_write_pending <= 1'b0;
+        end else if (set_dma_write_pending) begin 
+            dma_write_pending <= 1'b1;
+        end
+    end
+
+    logic recall_valid, clr_recall_valid, set_recall_valid;    
+    always @(posedge_clk or negedge rst) begin 
+        if (!rst || clr_recall_valid) begin 
+            recall_valid <= 1'b0;
+        end else if (set_recall_valid) begin 
+            recall_valid <= 1'b1;
+        end
+    end
+
+    //@TODO
+    llc_set_t req_in_stalled_set; 
+    llc_tag_t req_in_stalled_tag; 
+
 endmodule
