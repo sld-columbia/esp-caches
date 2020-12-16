3f9cf2c52230a71241a2a83a4ca52a2b379df0b4
Paolo Mantovani <paolo@cs.columbia.edu>
2020-12-16 12:24:31 -0500

    update Makefile and tcl scripts include paths

--- systemc/common/syn/project.tcl
+++ systemc/common/syn/project.tcl
@@ -1,3 +1,3 @@
 set ESP_ROOT_PATH $::env(ESP_ROOT)
 
-source ${ESP_ROOT_PATH}/accelerators/stratus_hls/common/syn/project.tcl
+source ${ESP_ROOT_PATH}/accelerators/stratus_hls/common/hls/project.tcl
