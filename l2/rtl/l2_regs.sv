b3a4d89b6f46e0190cbd19d6cac578aea83acc9b
Joseph Zuckerman <jzuck@cs.columbia.edu>
2021-09-20 13:34:27 -0400

    bug fixes in L2 for stable SMP execution
    -fix write to reqs buf from MIA
    -ensure forwards served from XMW after responses come
    -update state on FWD_HIT only if tag match

--- l2/rtl/l2_regs.sv
+++ l2/rtl/l2_regs.sv
@@ -125,8 +125,9 @@ module l2_regs (
             fwd_stall_ended <= 1'b0;
         end else if (clr_fwd_stall_ended) begin
             fwd_stall_ended <= 1'b0;
-        end else if (wr_en_put_reqs && fwd_stall && (fwd_stall_i == reqs_i
-                    || ((put_reqs_atomic || lr_to_xmw) && fwd_stall_i == reqs_atomic_i))) begin
+        end else if ((wr_en_put_reqs && fwd_stall && (fwd_stall_i == reqs_i
+                    || (put_reqs_atomic  && fwd_stall_i == reqs_atomic_i)))
+                    || (fwd_stall && lr_to_xmw && (fwd_stall_i == reqs_i))) begin
             fwd_stall_ended <= 1'b1;
         end
     end
