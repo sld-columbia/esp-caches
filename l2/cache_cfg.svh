653fd477066eea1092ce8cb84eb8068176ed43dc
Joseph Zuckerman <jzuck@cs.columbia.edu>
2023-06-05 22:08:47 -0400

    enable customization of cache line size

--- l2/cache_cfg.svh
+++ l2/cache_cfg.svh
@@ -4,12 +4,15 @@
 `define __CACHES_CFG_SVH__
 
 //define CPU ARCH here
-`define LEON
+//options LEON, ARIANE, IBEX
+`define ARIANE
+//cache line size
+//options: 4 (16B/128b), 5 (32B/256b), 6 (64B/512b), 7 (128B/1024b)
+`define CACHE_LINE_BYTES_LOG2 4
 
 `ifdef LEON
 `define BIG_ENDIAN
 `define BYTE_BITS    2
-`define WORD_BITS    2
 `define L2_WAYS      4
 `define L2_SETS      256
 `endif
@@ -18,7 +21,6 @@
 `define LLSC
 `define LITTLE_ENDIAN
 `define BYTE_BITS    3
-`define WORD_BITS    1
 `define L2_WAYS      4
 `define L2_SETS      256
 `endif
@@ -26,9 +28,10 @@
 `ifdef IBEX
 `define LITTLE_ENDIAN
 `define BYTE_BITS    2
-`define WORD_BITS    2
 `define L2_WAYS      4
 `define L2_SETS      256
 `endif
+
+`define WORD_BITS (`CACHE_LINE_BYTES_LOG2 - `BYTE_BITS)
 
 `endif // __CACHES_CFG_SVH__
--- llc/cache_cfg.svh
+++ llc/cache_cfg.svh
@@ -3,13 +3,20 @@
 `ifndef __CACHES_CFG_SVH__
 `define __CACHES_CFG_SVH__
 
-//set LITTLE_ENDIAN for Ariane, BIG_ENDIAN for Leon
+//endianness
+//set LITTLE_ENDIAN for Ariane and Ibex, BIG_ENDIAN for Leon
 `define BIG_ENDIAN
-//3 for Ariane; 2 for Leon
-`define BYTE_BITS    2
-//1 for Ariane; 2 for Leon
-`define WORD_BITS    2
+
+//word size
+//set 3 for Ariane; 2 for Leon, 2 for Ibex
+`define BYTE_BITS    3
+
+//cache line size
+//options: 4 (16B/128b), 5 (32B/256b), 6 (64B/512b), 7 (128B/1024b)
+`define CACHE_LINE_BYTES_LOG2 5
+
 `define LLC_WAYS     16
 `define LLC_SETS     512
+`define WORD_BITS    (`CACHE_LINE_BYTES_LOG2 - `BYTE_BITS)
 
 `endif // __CACHES_CFG_SVH__
