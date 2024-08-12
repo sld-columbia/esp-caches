b258c26196403fece4e40ed7e0e4ea0d6a51a0f4
Joseph Zuckerman <jzuck@cs.columbia.edu>
2024-08-12 12:32:01 -0400

    :bug: fix backpressure handling in cache interface controller

--- common/rtl/interface_controller.sv
+++ common/rtl/interface_controller.sv
@@ -7,53 +7,47 @@
 
 module interface_controller(
 
-    input logic clk, 
-    input logic rst, 
-    input logic ready_in, 
+    input logic clk,
+    input logic rst,
+    input logic ready_in,
     input logic valid_in,
-    output logic ready_out, 
-    output logic valid_out, 
+    output logic ready_out,
+    output logic valid_out,
     output logic valid_tmp
     );
 
-    localparam NOT_READY = 1'b0; 
-    localparam READY = 1'b1; 
+    localparam EMPTY = 1'b0;
+    localparam FULL = 1'b1;
 
-    logic ready_next; 
-    always_ff @(posedge clk or negedge rst) begin 
+    logic state, state_next;
+    always_ff @(posedge clk or negedge rst) begin
         if (!rst) begin
-            ready_out <= READY;
-        end else begin 
-            ready_out <= ready_next; 
+            state <= EMPTY;
+        end else begin
+            state <= state_next;
         end
     end
 
-    always_comb begin 
-        ready_next = ready_out;
-        case (ready_out)
-            NOT_READY : begin 
-                if (ready_in) begin 
-                    ready_next = READY; 
+    always_comb begin
+        state_next = state;
+        case (state)
+            EMPTY : begin
+                ready_out = 1'b1;
+                valid_tmp = 1'b0;
+                if (valid_in && !ready_in) begin
+                    state_next = FULL;
                 end
             end
-            READY : begin 
-                if (valid_in && !ready_in) begin 
-                    ready_next = NOT_READY;
+            FULL : begin
+                ready_out = 1'b0;
+                valid_tmp = 1'b1;
+                if (ready_in) begin
+                    state_next = EMPTY;
                 end
             end
         endcase
     end
 
-    always_ff @(posedge clk or negedge rst) begin 
-        if (!rst) begin 
-            valid_tmp <= NOT_READY;
-        end else if (ready_out && !ready_in) begin 
-            valid_tmp <= valid_in; 
-        end else if (valid_tmp && ready_in) begin 
-            valid_tmp <= valid_in; 
-        end
-    end
-    
-    assign valid_out = valid_in | valid_tmp; 
+    assign valid_out = valid_in | valid_tmp;
 
 endmodule
